abba22f9143fcf936e7210382c8cc7ce39e2016c
Lukas Bombach <lukas.bombach@googlemail.com>
2024-03-28 09:08:21 +0000

    ssr signals wip

--- ssr-signals.spec.tsx
+++ ssr-signals.spec.tsx
@@ -1,3 +1,12 @@
+import * as maverick from "@maverick-js/signals";
+
+function signal<T>(id: number, initialValue: T): maverick.WriteSignal<T> {
+  const s = maverick.signal(initialValue);
+  return s;
+}
+
+function el(element: string, props?: Record<string, unknown>, ...children: unknown[]) {}
+
 const Counter = async () => {
   const count = signal(0, Math.round(Math.random() * 100));
   return el(
