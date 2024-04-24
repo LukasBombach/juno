627a3e51395ade8ae829bbc85005e323959373b8
Lukas Bombach <lukas.bombach@googlemail.com>
2024-04-24 10:58:59 +0200

    better naming for binding and props

--- src/juno/dom.ts
+++ src/juno/dom.ts
@@ -1,8 +1,9 @@
 import { effect } from "@maverick-js/signals";
 
-type ElementBinding = Record<string, any>;
+export type DomBinding = [string, DomBindingProps];
+export type DomBindingProps = Record<string, any>;
 
-export function applyBinding(element: Element, binding: ElementBinding) {
+export function applyBinding(element: Element, binding: DomBindingProps) {
   Object.entries(binding).forEach(([key, value]) => {
     if (key.match(/^on[A-Z]/)) {
       element.addEventListener(key.slice(2).toLowerCase(), value);
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { applyBinding } from "juno/dom";
 import "style.css";
 
 import type { InstanceContext } from "juno/ssr";
+import type { DomBinding } from "juno/dom";
 
 const roots = getRoots();
 const instances = getState();
@@ -11,13 +12,13 @@ const instances = getState();
 // todo make this code nicer
 const ssr = roots.map(([id, root]) => [root, instances[id].state] as const);
 
-const Counter = (_props: null, ctx: InstanceContext) => {
+const Counter = (_props: null, ctx: InstanceContext): DomBinding[] => {
   const count = signal(ctx.state[0]);
   const increment = () => count.set(count() + 1);
   return [
     ["*:nth-child(1)", { onClick: increment }],
     ["*:nth-child(2)", { children: [count] }],
-  ] as const;
+  ];
 };
 
 // todos
@@ -30,8 +31,8 @@ for (const [root, state] of ssr) {
   const ctx = { state };
   const bindings = Counter(null, ctx);
 
-  bindings.forEach(([selector, binding]) => {
+  bindings.forEach(([selector, props]) => {
     const element = root.querySelector(selector)!;
-    applyBinding(element, binding);
+    applyBinding(element, props);
   });
 }
