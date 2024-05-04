49d581cafd120a009f1e3348d9a0bb013a150756
Lukas Bombach <lukas.bombach@googlemail.com>
2024-05-04 22:30:05 +0200

    vite inspect

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import Inspect from "vite-plugin-inspect";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [
+    tsconfigPaths(),
+    Inspect({
+      build: true,
+      outputDir: ".vite-inspect",
+    }),
+  ],
 });
