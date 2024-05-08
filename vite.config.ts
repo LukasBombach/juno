fa1cd1ff98967354b19278148986e4d9bdd4bf07
Lukas Bombach <lukas.bombach@googlemail.com>
2024-05-08 22:05:23 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import Inspect from "vite-plugin-inspect";
@@ -5,9 +7,16 @@ import Inspect from "vite-plugin-inspect";
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    Inspect({
-      build: true,
-      outputDir: ".vite-inspect",
-    }),
+    Inspect({ build: true, outputDir: ".vite-inspect" }),
+    {
+      name: "juno test apps",
+      configureServer(server) {
+        server.middlewares.use("/apps", async (req, res, next) => {
+          const fileName = req.url!.replace(/^\/(.*)/, "$1.jsx");
+          console.log((await import(`./apps/${fileName}`)).default);
+          next();
+        });
+      },
+    },
   ],
 });
