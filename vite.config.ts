4cc2ce81ce502ccb98032b9d52ad5100154e6972
Lukas Bombach <lukas.bombach@googlemail.com>
2024-05-11 12:33:07 +0200

    vite config mini cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import Inspect from "vite-plugin-inspect";
 import { renderToString } from "./src/juno/server";
-import juno from "./src/juno/vite";
+import Juno from "./src/juno/vite";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), Inspect({ dev: true }), juno()],
+  plugins: [tsconfigPaths(), Inspect(), Juno()],
 });
