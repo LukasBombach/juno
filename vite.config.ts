76677527a882374df7d2d04a999b396854e4f90e
Lukas Bombach <lukas.bombach@googlemail.com>
2024-05-11 12:17:14 +0200

    juno vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,27 +4,8 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import Inspect from "vite-plugin-inspect";
 import { renderToString } from "./src/juno/server";
+import juno from "./src/juno/vite";
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    Inspect({ build: true, outputDir: ".vite-inspect" }),
-    // {
-    //   name: "juno test apps",
-    //   configureServer(server) {
-    //     server.middlewares.use("/pages", async (req, res, next) => {
-    //       const urlPath = !req.url || req.url === "/" ? "/index" : req.url;
-    //       const fileName = urlPath.replace(/^\/(.*)/, "$1.jsx");
-    //       const filePath = path.resolve(__dirname, "pages", fileName);
-    //
-    //       const { default: Page } = await server.ssrLoadModule(filePath);
-    //       const vdom = Page();
-    //       const html = renderToString(vdom);
-    //       const viteHtml = await server.transformIndexHtml(urlPath, html);
-    //
-    //       res.end(viteHtml);
-    //     });
-    //   },
-    // },
-  ],
+  plugins: [tsconfigPaths(), Inspect({ dev: true }), juno()],
 });
