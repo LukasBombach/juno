a0561e67a1e9b19cda94d950806421bb6810dbc4
Lukas Bombach <lukas.bombach@googlemail.com>
2024-05-24 20:46:25 +0200

    x

--- app/client/index.ts
+++ app/client/index.ts
@@ -1,9 +1,9 @@
-import { importClientComponent, getSsrState, hydrate } from "juno/client";
+import { getSsrState, hydrate } from "juno/client";
 
 import type { Component } from "juno/compiler";
 
 const Page: Component = (ctx) => {
-  const count = ctx.signal(Math.floor(Math.random() * 100));
+  const count = ctx.signal(ctx.ssrData[Page.id][0]);
   return [
     ["1,2,1", { children: [count] }],
     ["1,2,2", { onClick: () => count.set(count() + 1) }],
