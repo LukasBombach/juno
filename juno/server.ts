1fd5763d5c21d297dfc323a3bd5b7ff7a0d3e844
Lukas Bombach <lukas.bombach@googlemail.com>
2024-05-25 10:37:18 +0200

    server.ts << renderToString.ts

--- juno/server.ts
+++ juno/server.ts
@@ -2,32 +2,15 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import express, { type Request } from "express";
 import { createServer as createViteServer, type ViteDevServer } from "vite";
-import { renderToString } from "juno/renderToString";
 import cliCursor from "cli-cursor";
 import chalk from "chalk";
 
+import type { ReactElement, ReactNode } from "react";
 import { signal as createSignal, type WriteSignal } from "@maverick-js/signals";
 
 export interface RenderContext {
   signal: <T>(value: T) => WriteSignal<T>;
   ssrData: any[];
-}
-
-export function createRenderContext(): RenderContext {
-  const signals: WriteSignal<any>[] = [];
-
-  const signal = <T>(value: T) => {
-    const s = createSignal<T>(value);
-    signals.push(s);
-    return s;
-  };
-
-  return {
-    signal,
-    get ssrData() {
-      return signals.map(s => s());
-    },
-  };
 }
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -81,3 +64,103 @@ function welcomeMessage() {
 
 await createServer();
 welcomeMessage();
+
+export function createRenderContext(): RenderContext {
+  const signals: WriteSignal<any>[] = [];
+
+  const signal = <T>(value: T) => {
+    const s = createSignal<T>(value);
+    signals.push(s);
+    return s;
+  };
+
+  return {
+    signal,
+    get ssrData() {
+      return signals.map(s => s());
+    },
+  };
+}
+
+export function renderToString(node: ReactNode | (() => ReactNode)): string {
+  if (typeof node === "boolean") {
+    return "";
+  }
+
+  if (node === null) {
+    return "";
+  }
+
+  if (typeof node === "undefined") {
+    return "";
+  }
+
+  if (typeof node === "string") {
+    return node;
+  }
+
+  if (typeof node === "number") {
+    return node.toString();
+  }
+
+  if (Array.isArray(node)) {
+    throw new Error("not yet implemented");
+  }
+
+  if (isReactElement(node)) {
+    if (typeof node.type === "string") {
+      const attrs = [];
+      const children = [];
+
+      const docType = node.type === "html" ? "<!DOCTYPE html>" : "";
+
+      for (let [key, val] of Object.entries(node.props)) {
+        if (key.match(/^on[A-Z]/)) {
+          // attrs.push(`${key}="--omit--"`);
+          continue;
+        }
+
+        if (typeof val === "function") {
+          val = val();
+        }
+
+        if (key === "className") {
+          key = "class";
+        }
+
+        if (key === "children") {
+          const vals = Array.isArray(val) ? val : [val];
+          children.push(...vals.map(renderToString));
+        } else {
+          attrs.push(`${key}="${val}"`);
+        }
+      }
+
+      if (node.props.children) {
+        return `${docType}<${[node.type, ...attrs].join(" ")}>${children.join("")}</${node.type}>`;
+      } else if (node.type === "script") {
+        return `${docType}<${[node.type, ...attrs].join(" ")}></${node.type}>`;
+      }
+      {
+        return `${docType}<${[node.type, ...attrs].join(" ")} />`;
+      }
+    }
+
+    if (typeof node.type == "function") {
+      return renderToString(node.type(node.props));
+    }
+  }
+
+  if (typeof node === "function") {
+    return renderToString(node());
+  }
+
+  console.warn(`Cannot handle react element type ${typeof node}`, node);
+  return "";
+}
+
+function isReactElement(
+  val: any
+): val is ReactElement<Record<string, unknown>, string | ((props: Record<string, unknown>) => ReactNode)> {
+  return typeof val === "object" && val !== null && "type" in val && "props" in val;
+}
