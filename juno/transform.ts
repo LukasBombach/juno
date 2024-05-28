3ee42d0d281d8554e1be66dac815b85be7383ef4
Lukas Bombach <lukas.bombach@googlemail.com>
2024-05-28 22:25:07 +0200

    migrated transformto ues generators

--- juno/transform.ts
+++ juno/transform.ts
@@ -16,24 +16,33 @@ export type NodeOfType<T extends NodeType> = Extract<Node, { type: T }>;
 
 export async function transformToClientCode(input: string): Promise<string> {
   const module = await parse(input, { syntax: "typescript", tsx: true });
+
+  for (const ret of find(module, "ReturnStatement")) {
+    console.log(ret);
+  }
+
   const { code: output } = await print(module);
   return output;
 }
 
-function find<T extends NodeType>(parent: Node, type: T): NodeOfType<T>[] {
-  const nodes = new Set<NodeOfType<T>>();
-  traverse(parent, (node) => node.type === type && nodes.add(node));
-  return Array.from(nodes);
+function* find<T extends NodeType>(parent: Node, type: T): Generator<NodeOfType<T>> {
+  for (const node of traverse(parent)) {
+    if (node.type === type) {
+      yield node;
+    }
+  }
 }
 
-function traverse(obj: any, callback: (node: any) => void) {
-  callback(obj);
-
+function* traverse(obj: any): Generator<any> {
   if (typeof obj === "object" && obj !== null) {
     if (Array.isArray(obj)) {
-      for (let i = 0; i < obj.length; i++) traverse(obj[i], callback);
+      for (let i = 0; i < obj.length; i++) yield* traverse(obj[i]);
     } else {
-      for (const key in obj) obj.hasOwnProperty(key) && traverse(obj[key], callback);
+      for (const key in obj) {
+        if (obj.hasOwnProperty(key)) yield* traverse(obj[key]);
+      }
     }
+  } else {
+    yield obj;
   }
 }
