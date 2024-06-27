67be0f2157f6e5f16710bd88db7b5d12d89167ae
Lukas Bombach <lukas.bombach@googlemail.com>
2024-06-27 22:24:17 +0200

    .jabsd-klnasd

--- juno/ast.ts
+++ juno/ast.ts
@@ -17,8 +17,7 @@ export type NodeType = Node["type"];
 export type NodeOfType<T extends NodeType> = Extract<Node, { type: T }>;
 
 type TypeQuery<T extends NodeType> = `${string}type=${T}`;
-
-type QueryResult<P extends string> = P extends TypeQuery<infer T> ? NodeOfType<T> | undefined : Node | undefined;
+type QueryResult<P extends string> = P extends TypeQuery<infer T> ? Api<NodeOfType<T>> : Api<Node>;
 
 export async function parse(src: string, options?: ParseOptions): Promise<Api<Module>> {
   const module = await swcparse(src, options);
@@ -32,7 +31,52 @@ class Api<T extends Node> {
     return this.node.type === type;
   }
 
-  query<T extends string>(query: T): QueryResult<T> {}
+  /* static isNode(value: unknown): value is Node {
+    return typeof value === "object" && value !== null && "type" in value;
+  }
+
+  has(prop: PropertyKey): prop is keyof T {
+    return this.node.hasOwnProperty(prop);
+  }
+
+  isNode(prop: keyof T): boolean {
+    return Api.isNode(this.node[prop]);
+  } */
+
+  getNode(prop: string): Node | undefined {
+    if (
+      prop in this.node &&
+      typeof this.node[prop] === "object" &&
+      this.node[prop] !== null &&
+      "type" in this.node[prop]
+    ) {
+      return this.node[prop];
+    }
+    return undefined;
+  }
+
+  query<T extends string>(query: T): QueryResult<T> | undefined {
+    const parts = query.split(".");
+    let node: Node = this.node;
+
+    for (const part of parts) {
+      const maybeNode = this.getNode(part);
+      if (maybeNode) {
+        node = maybeNode;
+      } else {
+        return undefined;
+      }
+    }
+
+    return node;
+
+    /* const property = query.split(".")[0];
+    const [key, value] = property.split("=");
+
+    if (key && value && this.node[key as keyof Node] === value) {
+      return this.node as any; // TODO ANY;
+    } */
+  }
 
   *find<T extends NodeType>(type: T): Generator<Api<NodeOfType<T>>> {
     for (const node of this.traverse(this.node)) {
--- juno/transform2.ts
+++ juno/transform2.ts
@@ -4,6 +4,6 @@ export async function transformToClientCode(src: string): Promise<string> {
   const module = await parse(src);
 
   for (const fn of module.find("FunctionExpression")) {
-    const contextParameter = fn.query("params[0].type=Identifier");
+    const contextParameter = fn.query("params.0.type=Identifier");
   }
 }
