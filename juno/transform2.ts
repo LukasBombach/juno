42f9f639f6300af534b26416b918859f40819da6
Lukas Bombach <lukas.bombach@googlemail.com>
2024-06-28 17:13:05 +0200

    concision

--- juno/transform2.ts
+++ juno/transform2.ts
@@ -4,14 +4,14 @@ export async function transformToClientCode2(src: string): Promise<string> {
   const module = await parse(src, { syntax: "typescript", tsx: true });
 
   for (const fn of module.find("FunctionExpression")) {
-    const ctxParam = fn.query("params[0].pat.type=Identifier");
-    const usages = ctxParam
+    const usages = fn
+      .query("params[0].pat.type=Identifier")
       ?.findUsages()
       .map(toParent("MemberExpression"))
       .filter(byQuery("property.type=Identifier&value=signal"))
       .map(toParent("CallExpression"));
 
-    console.log(usages?.length);
+    console.log(usages?.map(usage => usage?.node));
   }
 
   return src;
