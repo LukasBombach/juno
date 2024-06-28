23de67a38a472f95c013942c64087ace015681a7
Lukas Bombach <lukas.bombach@googlemail.com>
2024-06-28 14:20:45 +0200

    parents map and findUsages

--- juno/ast.ts
+++ juno/ast.ts
@@ -21,14 +21,16 @@ type QueryResult<P extends string> = P extends TypeQuery<infer T> ? Api<NodeOfTy
 
 export async function parse(src: string, options?: ParseOptions): Promise<Api<Module>> {
   const module = await swcparse(src, options);
-  return new Api(module);
+  return new Api(module, Api.mapParents(module));
 }
 
 class Api<T extends Node> {
-  constructor(public node: T) {}
+  constructor(public readonly node: T, private readonly parents: Map<Node, Node>) {}
 
-  is<T extends NodeType>(type: T): this is Api<NodeOfType<T>> {
-    return this.node.type === type;
+  is<T extends NodeType>(type: T): this is Api<NodeOfType<T>>;
+  is<T extends NodeType>(...type: T[]): this is Api<NodeOfType<T>>;
+  is<T extends NodeType>(type: T | T[]): this is Api<NodeOfType<T>> {
+    return Array.isArray(type) ? type.some(t => (this.node.type = t)) : this.node.type === type;
   }
 
   query<T extends string>(query: T): QueryResult<T> | undefined {
@@ -56,9 +58,32 @@ class Api<T extends Node> {
     }
 
     if (Api.isNode(node)) {
-      return new Api(node) as QueryResult<T>;
+      return new Api(node, this.parents) as QueryResult<T>;
     } else {
       return undefined;
+    }
+  }
+
+  findScope(): Api<t.FunctionExpression | t.Module> {
+    return [...this.findParents()].filter((node): node is Api<t.FunctionExpression | t.Module> =>
+      node.is("FunctionExpression", "Module")
+    )[0];
+  }
+
+  *findParents(): Generator<Api<Node>> {
+    let parent = this.parents.get(this.node);
+    while (parent) {
+      yield new Api(parent, this.parents);
+      parent = this.parents.get(parent);
+    }
+  }
+
+  *findUsages(): Generator<Api<t.Identifier>> {
+    if (!this.is("Identifier")) return;
+    const scope = this.findScope();
+    console.log(scope.node.type);
+    for (const id of scope.find("Identifier")) {
+      if (id.node !== this.node && id.node.value === this.node.value) yield id;
     }
   }
 
@@ -74,9 +99,32 @@ class Api<T extends Node> {
     return typeof value === "object" && value !== null && "type" in value;
   }
 
+  static mapParents(node: Node, parents = new Map<Node, Node>()): Map<Node, Node> {
+    let parent = node;
+    let property: keyof typeof parent;
+
+    for (property in parent) {
+      const child = parent[property];
+      if (Api.isNode(child)) {
+        parents.set(child, parent);
+        this.mapParents(child, parents);
+      }
+      if (Array.isArray(child)) {
+        for (const nthChild of child) {
+          if (Api.isNode(nthChild)) {
+            parents.set(nthChild, parent);
+            this.mapParents(nthChild, parents);
+          }
+        }
+      }
+    }
+
+    return parents;
+  }
+
   private *traverse(obj: any): Generator<Api<Node>> {
     if (typeof obj === "object" && obj !== null && "type" in obj) {
-      yield new Api(obj);
+      yield new Api(obj, this.parents);
     }
 
     if (typeof obj === "object" && obj !== null) {
--- juno/transform2.ts
+++ juno/transform2.ts
@@ -5,9 +5,7 @@ export async function transformToClientCode2(src: string): Promise<string> {
 
   for (const fn of module.find("FunctionExpression")) {
     const ctxParam = fn.query("params.0.pat.type=Identifier");
-    const usages = [...fn.find("Identifier")].filter(
-      id => id.node !== ctxParam?.node && id.node.value === ctxParam?.node.value
-    );
+    const usages = [...ctxParam!.findUsages()];
 
     console.log(usages.length);
   }
