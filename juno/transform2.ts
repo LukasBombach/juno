d84eb0569cf16f1fcaf60e44e4efc56f8d1e4624
Lukas Bombach <lukas.bombach@googlemail.com>
2024-06-28 13:02:40 +0200

    wip

--- juno/transform2.ts
+++ juno/transform2.ts
@@ -4,9 +4,12 @@ export async function transformToClientCode2(src: string): Promise<string> {
   const module = await parse(src, { syntax: "typescript", tsx: true });
 
   for (const fn of module.find("FunctionExpression")) {
-    const contextParameter = fn.query("params.0.pat.type=Identifier");
+    const ctxParam = fn.query("params.0.pat.type=Identifier");
+    const usages = [...fn.find("Identifier")].filter(
+      id => id.node !== ctxParam?.node && id.node.value === ctxParam?.node.value
+    );
 
-    console.log(contextParameter);
+    console.log(usages.length);
   }
 
   return src;
