9e6a6e7be5c37a035350d801c4aefcce773a5fc6
Lukas Bombach <lukas.bombach@googlemail.com>
2024-07-06 09:37:32 +0200

    doodling with pipe calls

---
+++ juno/transform-fp2.ts
@@ -0,0 +1,36 @@
+import { pipe } from "fp-ts/function";
+import { parse } from "juno/ast";
+
+export async function transformToClientCode2(src: string): Promise<string> {
+  const module = await parse(src, { syntax: "typescript", tsx: true });
+
+  for (const fn of module.find("FunctionExpression")) {
+    const signalCalls = pipe(
+      fn,
+      get("params[0].pat.type=Identifier"),
+      parent("MemberExpression"),
+      filter("property.type=Identifier&value=signal"),
+      parent("CallExpression"),
+      filter(nonNullable)
+    );
+
+    const signalCalls2 = pipe(
+      fn,
+      functionParams(0),
+      filter({ type: "Identifier" }),
+      references(),
+      parent({ type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
+      parent({ type: "CallExpression" })
+    );
+
+    const signalCalls3 = pipe(
+      fn,
+      contextParameter(),
+      references(),
+      parent({ type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
+      parent({ type: "CallExpression" })
+    );
+  }
+
+  return src;
+}
