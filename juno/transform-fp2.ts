707e669fe9f2accc1e2330a4ccd1bcdc886c918a
Lukas Bombach <lukas.bombach@googlemail.com>
2024-07-07 21:33:48 +0200

    fp2

--- juno/transform-fp2.ts
+++ juno/transform-fp2.ts
@@ -1,12 +1,14 @@
 import { pipe } from "fp-ts/function";
 import { parse } from "juno/ast";
 
+import type * as t from "@swc/types";
+
 export async function transformToClientCode2(src: string): Promise<string> {
   const module = await parse(src, { syntax: "typescript", tsx: true });
 
   for (const fn of module.find("FunctionExpression")) {
     const signalCalls = pipe(
-      fn,
+      fn.node,
       children({ type: "Parameter", index: 0, pat: { type: "Identifier" } }),
       references(),
       parent({ type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
@@ -15,4 +17,21 @@ export async function transformToClientCode2(src: string): Promise<string> {
   }
 
   return src;
+}
+
+type Query = Record<string, unknown>;
+
+type Node =
+  | t.Program
+  | t.Statement
+  | t.Expression
+  | t.Declaration
+  | t.SpreadElement
+  | t.VariableDeclarator
+  | t.JSXOpeningElement
+  | t.JSXAttribute
+  | t.JSXExpressionContainer;
+
+function children(query: Query) {
+  return (node: Node): Node[] => node.children.filter(byQuery(query));
 }
