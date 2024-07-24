e9fb7774bc8620801133ce54199f8f87e7f9ca8e
Lukas Bombach <lukas.bombach@googlemail.com>
2024-07-24 21:05:29 +0200

    no effing clue

--- juno-ast/get.ts
+++ juno-ast/get.ts
@@ -4,3 +4,9 @@ import type { Node } from "juno-ast/parse";
 export function get<N extends Node, P extends keyof N>(name: P) {
   return map((val: N) => val[name]);
 }
+
+import type { Option } from "juno-ast/pipe";
+
+export function get2<N extends Node, P extends keyof N>(name: P): (node: Option<N>) => Option<N[P]> {
+  return node => node?.[name];
+}
--- juno/transform-fp.ts
+++ juno/transform-fp.ts
@@ -2,7 +2,7 @@ import { pipe } from "juno-ast/pipe";
 import { parse } from "juno-ast/parse";
 import { findFirst, findAll, parent } from "juno-ast/find2";
 import { getReferences } from "juno-ast/refs2";
-import { get } from "juno-ast/get";
+import { get, get2 } from "juno-ast/get";
 import type { PipeApi } from "juno-ast/pipe";
 import type { Node } from "juno-ast/parse";
 
@@ -13,7 +13,7 @@ export async function transformToClientCode(src: string): Promise<string> {
     const signalCalls = pipe(
       fn,
       findFirst({ type: "Parameter", index: 0, pat: { type: "Identifier" } }),
-      get("pat"),
+      get2("pat"),
       getReferences(),
       parent({ type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
       parent({ type: "CallExpression" }),
