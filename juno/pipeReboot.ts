4cfaeb8e53adf6e1b21fad95937e07a27c1a965d
Lukas Bombach <lukas.bombach@googlemail.com>
2024-07-25 16:06:31 +0200

    reboot

---
+++ juno/pipeReboot.ts
@@ -0,0 +1,82 @@
+import { traverse } from "juno-ast/traverse";
+import type { Node } from "juno-ast/parse";
+
+export interface PipeApi {
+  pipe: typeof pipe;
+  ancestors: (from: Node) => Generator<Node>;
+}
+
+export function pipe<A>(a: A): A;
+export function pipe<A, B>(a: A, ab: (a: A, api: PipeApi) => B): B;
+export function pipe<A, B, C>(a: A, ab: (a: A, api: PipeApi) => B, bc: (b: B, api: PipeApi) => C): C;
+export function pipe<A, B, C, D>(
+  a: A,
+  ab: (a: A, api: PipeApi) => B,
+  bc: (b: B, api: PipeApi) => C,
+  cd: (c: C, api: PipeApi) => D
+): D;
+export function pipe<A, B, C, D, E>(
+  a: A,
+  ab: (a: A, api: PipeApi) => B,
+  bc: (b: B, api: PipeApi) => C,
+  cd: (c: C, api: PipeApi) => D,
+  de: (d: D, api: PipeApi) => E
+): E;
+export function pipe<A, B, C, D, E, F>(
+  a: A,
+  ab: (a: A, api: PipeApi) => B,
+  bc: (b: B, api: PipeApi) => C,
+  cd: (c: C, api: PipeApi) => D,
+  de: (d: D, api: PipeApi) => E,
+  ef: (e: E, api: PipeApi) => F
+): F;
+export function pipe<A, B, C, D, E, F, G>(
+  a: A,
+  ab: (a: A, api: PipeApi) => B,
+  bc: (b: B, api: PipeApi) => C,
+  cd: (c: C, api: PipeApi) => D,
+  de: (d: D, api: PipeApi) => E,
+  ef: (e: E, api: PipeApi) => F,
+  fg: (f: F, api: PipeApi) => G
+): G;
+export function pipe<A, B, C, D, E, F, G, H>(
+  a: A,
+  ab: (a: A, api: PipeApi) => B,
+  bc: (b: B, api: PipeApi) => C,
+  cd: (c: C, api: PipeApi) => D,
+  de: (d: D, api: PipeApi) => E,
+  ef: (e: E, api: PipeApi) => F,
+  fg: (f: F, api: PipeApi) => G,
+  gh: (g: G, api: PipeApi) => H
+): H;
+export function pipe<A, B, C, D, E, F, G, H, I>(
+  a: A,
+  ab: (a: A, api: PipeApi) => B,
+  bc: (b: B, api: PipeApi) => C,
+  cd: (c: C, api: PipeApi) => D,
+  de: (d: D, api: PipeApi) => E,
+  ef: (e: E, api: PipeApi) => F,
+  fg: (f: F, api: PipeApi) => G,
+  gh: (g: G, api: PipeApi) => H,
+  hi: (h: H, api: PipeApi) => I
+): I;
+export function pipe(a: Node<"Module">, ...fns: ((val: any, api: PipeApi) => any)[]): unknown {
+  let val = a;
+  let module = a;
+  const api = { pipe, ancestors };
+
+  const ancestorMap = new Map<Node, Node>();
+  for (const [child, parent] of traverse(module)) ancestorMap.set(child, parent);
+  function* ancestors(node: Node): Generator<Node> {
+    let current: Node | undefined = ancestorMap.get(node);
+    while (current) {
+      yield current;
+      current = ancestorMap.get(current);
+    }
+  }
+
+  for (let i = 0; i < fns.length; i++) {
+    val = fns[i](val, api);
+  }
+  return val;
+}
--- juno/transform-fp.ts
+++ juno/transform-fp.ts
@@ -1,39 +1,30 @@
-import { pipe } from "juno-ast/pipe";
+import { pipe } from "./pipeReboot";
 import { parse } from "juno-ast/parse";
 import { findFirst, findAll, parent } from "juno-ast/find2";
-import { getReferences } from "juno-ast/refs2";
-import { get, get2 } from "juno-ast/get";
+// import { getReferences } from "juno-ast/refs2";
+// import { get, get2 } from "juno-ast/get";
 import type { PipeApi } from "juno-ast/pipe";
 import type { Node } from "juno-ast/parse";
 
 export async function transformToClientCode(src: string): Promise<string> {
   const module = await parse(src, { syntax: "typescript", tsx: true });
 
-  pipe(module)(
+  const signalCalls = pipe(
     module,
     findAll({ type: "FunctionExpression" }),
-    (fn: Node<"FunctionExpression">[], { pipe }: PipeApi): void => {
-      const signalCalls = pipe(
-        fn,
-        findFirst({ type: "Parameter", index: 0, pat: { type: "Identifier" } }),
-        get("pat"),
-        getReferences(),
-        parent({ type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
-        parent({ type: "CallExpression" }),
-        get("arguments")
-        // replace("arguments", "$CTX.ssrData[$I]")
-      );
-
-      const x = pipe(
-        fn,
-        findFirst({ type: "Parameter", index: 0, pat: { type: "Identifier" } }),
-        get("pat")
-        // getReferences()
-      );
-
-      console.log(signalCalls);
-    }
+    findFirst({ type: "Parameter", index: 0, pat: { type: "Identifier" } })
   );
+
+  // findAll({ type: "FunctionExpression" })
+  // findFirst({ type: "Parameter", index: 0, pat: { type: "Identifier" } })
+  //get("pat"),
+  //getReferences(),
+  //parent({ type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
+  //parent({ type: "CallExpression" }),
+  //get("arguments")
+  // replace("arguments", "$CTX.ssrData[$I]")
+
+  console.log(signalCalls);
 
   return src;
 }
