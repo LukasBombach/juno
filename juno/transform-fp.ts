53f5172bcec56f933c2af2dffe3e55d16e30fdff
Lukas Bombach <lukas.bombach@googlemail.com>
2024-07-25 21:02:30 +0200

    got the nesting

--- juno/transform-fp.ts
+++ juno/transform-fp.ts
@@ -4,16 +4,19 @@ import { parse } from "juno-ast/parse";
 export async function transformToClientCode(src: string): Promise<string> {
   const module = await parse(src, { syntax: "typescript", tsx: true });
 
-  const signalCalls = pipe(
-    module,
-    findAll({ type: "FunctionExpression" }),
-    findFirst({ type: "Parameter", index: 0, pat: { type: "Identifier" } }),
-    get("pat"),
-    getReferences(),
-    parent({ type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
-    parent({ type: "CallExpression" }),
-    get("arguments")
-  );
+  const signalCalls = pipe(module, findAll({ type: "FunctionExpression" }), (functions, { pipe }) => {
+    return functions.map((func) => {
+      return pipe(
+        func,
+        findFirst({ type: "Parameter", index: 0, pat: { type: "Identifier" } }),
+        get("pat"),
+        getReferences(),
+        parent({ type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
+        parent({ type: "CallExpression" }),
+        get("arguments")
+      );
+    });
+  });
 
   console.log(signalCalls);
 
