fcc2fff370d42eb09ad156df81813c5710432349
Lukas Bombach <lukas.bombach@googlemail.com>
2024-09-10 17:23:36 +0200

    shrug

--- juno/transform-fp.ts
+++ juno/transform-fp.ts
@@ -20,49 +20,53 @@ import type { Node } from "juno-ast/parse";
 export async function transformToClientCode(src: string): Promise<string> {
   const module = await parse(src, { syntax: "typescript", tsx: true });
 
-  for (const func of findAll({ type: "FunctionExpression" })(module)) {
-    const contextParam = pipe(
-      func,
-      findFirst({ type: "Parameter", index: 0, pat: { type: "Identifier" } }),
-      getProp("pat"),
-      is("Identifier")
-    );
+  pipe(
+    module,
+    findAll({ type: "FunctionExpression" }),
+    forEach(fn => {
+      const contextParam = pipe(
+        fn,
+        findFirst({ type: "Parameter", index: 0, pat: { type: "Identifier" } }),
+        getProp("pat"),
+        is("Identifier")
+      );
 
-    pipe(
-      contextParam,
-      getReferences(),
-      parent({ type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
-      parent({ type: "CallExpression" }),
-      getProp("arguments"),
-      first(),
-      replace("ctx.ssrData[i]", i => ({ ctx: contextParam?.value, i }))
-    );
+      pipe(
+        contextParam,
+        getReferences(),
+        parent({ type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
+        parent({ type: "CallExpression" }),
+        getProp("arguments"),
+        first(),
+        replace("ctx.ssrData[i]", i => ({ ctx: contextParam?.value, i }))
+      );
 
-    pipe(
-      func,
-      findAll({ type: "ReturnStatement" }),
-      forEach(returnStatement => {
-        pipe(
-          returnStatement,
-          getProp("argument"),
-          replace(
-            createReactiveInstructions(
-              pipe(
-                returnStatement,
-                findAll({ type: "JSXAttribute", name: { value: /^on[A-Z]/ } }),
-                findAll({ type: "Identifier" }),
-                flat(),
-                getUsages(),
-                flat(),
-                parent({ type: "JSXElement" }),
-                unique()
+      pipe(
+        fn,
+        findAll({ type: "ReturnStatement" }),
+        forEach(returnStatement => {
+          pipe(
+            returnStatement,
+            getProp("argument"),
+            replace(
+              createReactiveInstructions(
+                pipe(
+                  returnStatement,
+                  findAll({ type: "JSXAttribute", name: { value: /^on[A-Z]/ } }),
+                  findAll({ type: "Identifier" }),
+                  flat(),
+                  getUsages(),
+                  flat(),
+                  parent({ type: "JSXElement" }),
+                  unique()
+                )
               )
             )
-          )
-        );
-      })
-    );
-  }
+          );
+        })
+      );
+    })
+  );
 
   return src;
 }
