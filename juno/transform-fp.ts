109f25ecd887bf777723f53e8a1129118887cbe3
Lukas Bombach <lukas.bombach@googlemail.com>
2024-09-28 11:44:31 +0200

    x

--- juno/transform-fp.ts
+++ juno/transform-fp.ts
@@ -21,54 +21,48 @@ export async function transformToClientCode(src: string): Promise<string> {
    * Find all signal() initializations and replace their initial values with the SSR data
    * ctx.signal(xxx)   →   ctx.signal(ctx.ssrData[i])
    */
-  pipe(
-    functions,
-    forEach((fn) => {
-      pipe(
-        fn,
-        findFirst({ type: "Parameter", index: 0, pat: { type: "Identifier" } }),
-        getProp("pat"),
-        is("Identifier"),
-        forEach((ctx) =>
-          pipe(
-            ctx,
-            getReferences(),
-            parent({ type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
-            parent({ type: "CallExpression" }),
-            getProp("arguments"),
-            first(),
-            replace((_, i) => `${ctx?.value}.ssrData[${i}]`)
-          )
+  functions.forEach((fn) => {
+    pipe(
+      fn,
+      findFirst({ type: "Parameter", index: 0, pat: { type: "Identifier" } }),
+      getProp("pat"),
+      is("Identifier"),
+      forEach((ctx) =>
+        pipe(
+          ctx,
+          getReferences(),
+          parent({ type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
+          parent({ type: "CallExpression" }),
+          getProp("arguments"),
+          first(),
+          replace((_, i) => `${ctx?.value}.ssrData[${i}]`)
         )
-      );
-    })
-  );
+      )
+    );
+  });
 
   /**
    * Find all return statements in the function and replace the returned JSX elements with an array
    * of extracted info that is relevant for hydration
    * return <div onClick={increment}>Count: {count}</div>   →   return [ { path: [1], onClick: increment, children: [7, count] } ]
    */
-  pipe(
-    functions,
-    forEach((fn) => {
-      pipe(
-        fn,
-        findAll({ type: "ReturnStatement" }),
-        replace((returnStatement) => {
-          pipe(
-            returnStatement,
-            findAll({ type: "JSXAttribute", name: { value: /^on[A-Z]/ } }),
-            findAll({ type: "Identifier" }),
-            flat(),
-            getUsages(),
-            flat()
-          );
-          return "";
-        })
-      );
-    })
-  );
+  functions.forEach((fn) => {
+    pipe(
+      fn,
+      findAll({ type: "ReturnStatement" }),
+      replace((returnStatement) => {
+        pipe(
+          returnStatement,
+          findAll({ type: "JSXAttribute", name: { value: /^on[A-Z]/ } }),
+          findAll({ type: "Identifier" }),
+          flat(),
+          getUsages(),
+          flat()
+        );
+        return "";
+      })
+    );
+  });
 
   return src;
 }
