eb8adda6a7e12d7bb73912fb165f01bc64e350d0
Lukas Bombach <lukas.bombach@googlemail.com>
2024-10-06 10:37:53 +0200

    updated the thing

--- juno/transform-fp.ts
+++ juno/transform-fp.ts
@@ -21,22 +21,23 @@ export async function transformToClientCode(src: string): Promise<string> {
    * Find all signal() initializations and replace their initial values with the SSR data
    * ctx.signal(xxx)   →   ctx.signal(ctx.ssrData[i])
    */
-  functions.forEach((fn) => {
-    pipe(
+  functions.forEach(fn => {
+    const ctxParam = pipe(
       fn,
       findFirst({ type: "Parameter", index: 0, pat: { type: "Identifier" } }),
       getProp("pat"),
-      is("Identifier"),
-      forEach((ctx) =>
-        pipe(
-          fn,
-          findAll({ type: "Identifier", value: ctx.value }),
-          parent(fn, { type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
-          parent(fn, { type: "CallExpression" }),
-          (calls) => calls.map((call) => call.arguments[0].expression), // todo custom function - or not todo, it's actually cool I can do custom stuff here
-          replace((_, i) => `${ctx.value}.ssrData[${i}]`)
-        )
-      )
+      is("Identifier")
+    );
+
+    if (!ctxParam) return;
+
+    pipe(
+      fn,
+      findAll({ type: "Identifier", value: ctxParam.value }),
+      parent(fn, { type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
+      parent(fn, { type: "CallExpression" }),
+      calls => calls.map(call => call.arguments[0].expression), // todo custom function - or not todo, it's actually cool I can do custom stuff here
+      replace((_, i) => `${ctxParam.value}.ssrData[${i}]`)
     );
   });
 
@@ -45,11 +46,11 @@ export async function transformToClientCode(src: string): Promise<string> {
    * of extracted info that is relevant for hydration
    * return <div onClick={increment}>Count: {count}</div>   →   return [ { path: [1], onClick: increment, children: [7, count] } ]
    */
-  functions.forEach((fn) => {
+  functions.forEach(fn => {
     pipe(
       fn,
       findAll({ type: "ReturnStatement" }),
-      replace((returnStatement) =>
+      replace(returnStatement =>
         pipe(
           returnStatement,
           findAll({ type: "JSXAttribute", name: { value: /^on[A-Z]/ } }),
