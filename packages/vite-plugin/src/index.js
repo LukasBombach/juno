429e95b3ca26d6b362748aab12056fdc83ee9798
Lukas Bombach <lukas.bombach@googlemail.com>
2024-11-08 11:25:49 +0100

    configure server

--- packages/vite-plugin/src/index.js
+++ packages/vite-plugin/src/index.js
@@ -6,8 +6,15 @@ export function juno() {
     name: "vite-plugin-juno",
     enforce: "pre",
 
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        console.log(req.url);
+        next();
+      });
+    },
+
     async resolveId(importee, importer, opts) {
-      console.log("resolveId", { importee, importer, opts });
+      // console.log("resolveId", { importee, importer, opts });
     },
   };
 }
