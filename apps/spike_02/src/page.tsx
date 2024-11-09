e458ec6829969eb3395cb7d77954002c45ee48b3
Lukas Bombach <lukas.bombach@googlemail.com>
2024-11-09 12:22:06 +0100

    ssr wip

---
+++ apps/spike_02/src/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>juno</title>
+      </head>
+      <body>hello juno html</body>
+    </html>
+  );
+}
