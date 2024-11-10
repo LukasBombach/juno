f59c6f5c5f907335968f0310d53926c9cbe322e0
Lukas Bombach <lukas.bombach@googlemail.com>
2024-11-10 22:37:37 +0100

    transform does somtheig

--- packages/transform/transform.ts
+++ packages/transform/transform.ts
@@ -13,13 +13,16 @@ const span = {
 
 export async function transform(src: string): Promise<string> {
   const module = await parse(src, { syntax: "typescript", tsx: true });
-  const functions = pipe(module, findAll({ type: "FunctionDeclaration" }));
+  const functions = [
+    ...pipe(module, findAll({ type: "FunctionDeclaration" })),
+    ...pipe(module, findAll({ type: "FunctionExpression" })),
+  ];
 
   /**
    * Find all signal() initializations and replace their initial values with the SSR data
    * ctx.signal(xxx)   →   ctx.signal(ctx.ssrData[i])
    */
-  functions.forEach((fn) => {
+  functions.forEach(fn => {
     const ctxParam = pipe(fn, findFirst({ type: "Parameter", index: 0 }), getProp("pat"), is("Identifier"));
 
     if (!ctxParam) return;
@@ -29,7 +32,7 @@ export async function transform(src: string): Promise<string> {
       findAll({ type: "Identifier", value: ctxParam.value }),
       parent(fn, { type: "MemberExpression", property: { type: "Identifier", value: "signal" } }),
       parent(fn, { type: "CallExpression" }),
-      (calls) => calls.map((call) => call?.arguments[0].expression).filter(Boolean), // todo custom function - or not todo, it's actually cool I can do custom stuff here
+      calls => calls.map(call => call?.arguments[0].expression).filter(Boolean), // todo custom function - or not todo, it's actually cool I can do custom stuff here
       replace(fn, (_, i) => {
         return {
           type: "MemberExpression",
@@ -40,12 +43,14 @@ export async function transform(src: string): Promise<string> {
             object: {
               type: "Identifier",
               span,
+              ctxt: 0,
               value: ctxParam.value,
               optional: false,
             },
             property: {
               type: "Identifier",
               span,
+              ctxt: 0,
               value: "ssrData",
               optional: false,
             },
@@ -78,11 +83,11 @@ export async function transform(src: string): Promise<string> {
    * return <div onClick={increment}>Count: {count}</div>   →   return [ { path: [1], onClick: increment, children: [7, count] } ]
    */
 
-  functions.forEach((fn) => {
+  functions.forEach(fn => {
     pipe(
       fn,
       findAll({ type: "ReturnStatement" }),
-      replace(fn, (returnStatement) => {
+      replace(fn, returnStatement => {
         return transformHydrations(returnStatement);
       })
     );
