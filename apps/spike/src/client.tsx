8111c10fb9747f20db156880fe3b793c489fed9a
Lukas Bombach <lukas.bombach@googlemail.com>
2024-11-10 22:50:08 +0100

    the basic transform setup WORKS

--- apps/spike/src/client.tsx
+++ apps/spike/src/client.tsx
@@ -1,4 +1,7 @@
 import { hydrate } from "@juno/client";
+import { signal } from "@maverick-js/signals";
 import Page from "./page";
+
+console.log(Page({ signal, ssrData: [] }));
 
 hydrate(document, <Page />);
