a4269600ef2f84c0e5889697f1008cf82a2945d2
Lukas Bombach <lukas.bombach@googlemail.com>
2024-11-30 11:18:22 +0100

    wip

--- apps/spike/vite.config.ts
+++ apps/spike/vite.config.ts
@@ -3,6 +3,6 @@ import inspect from "vite-plugin-inspect";
 import juno from "@juno/vite-plugin-juno";
 
 export default defineConfig({
-  clearScreen: false,
   plugins: [inspect(), juno()],
+  clearScreen: false,
 });
