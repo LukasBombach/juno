b9aeaa0ea7842f2930ff71cd6888c56281af629b
Lukas Bombach <lukas.bombach@googlemail.com>
2024-12-02 17:41:51 +0100

    mock transform server code

--- packages/transform_server/package.ts
+++ packages/transform_server/package.ts
@@ -3,5 +3,7 @@ import { parse, print } from "@juno/parse";
 export async function transformServer(src: string): Promise<string> {
   const module = await parse(src);
 
+  getJsxElements(module).filter(shouldBeHydrated).forEach(appendHydrationMarker);
+
   return await print(module);
 }
