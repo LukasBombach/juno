bb8937477995e302b066bd3f619301705dd2195d
Lukas Bombach <lukas.bombach@googlemail.com>
2024-12-21 22:28:34 +0100

    cleanup

--- packages/pipe/filterInteractive.ts
+++ packages/pipe/filterInteractive.ts
@@ -5,8 +5,6 @@ import type { Node, t } from "@juno/parse";
 export function filterInteractive() {
   return (elements: Node<"JSXElement">[]): Node<"JSXElement">[] => {
     return filterInteractiveJsxBySimpleIdentifierStrategy(elements);
-    // const eventHandlers = elements.flatMap(getAttributes).filter(isNotSpread).filter(isEventHandler);
-    // const identifiers = eventHandlers.flatMap(getIdentifiers).filter;
   };
 }
 
@@ -15,14 +13,11 @@ function filterInteractiveJsxBySimpleIdentifierStrategy(elements: Node<"JSXEleme
     elements,
     flatMap(el => el.opening.attributes),
     filter(attr => attr.type === "JSXAttribute"),
-    filter(attr => !!getName(attr)?.match(/^on[A-Z]/))
+    filter(attr => !!getName(attr)?.match(/^on[A-Z]/)),
+    flatMap(attr => getIdentifiers(attr))
   );
 
   throw new Error("Not implemented: filterInteractiveJsxBySimpleIdentifierStrategy");
-}
-
-function map<T, R>(fn: (value: T) => R): (arr: T[]) => R[] {
-  return arr => arr.map(fn);
 }
 
 function flatMap<T, R>(fn: (value: T) => R[]): (arr: T[]) => R[] {
@@ -35,26 +30,10 @@ function filter(predicate: (value: any) => boolean) {
   return (arr: any[]) => arr.filter(predicate);
 }
 
-function getAttributes(element: t.JSXElement): t.JSXAttributeOrSpread[] {
-  return element.opening.attributes;
-}
-
-function isNotSpread(attr: t.JSXAttributeOrSpread): attr is t.JSXAttribute {
-  return attr.type === "JSXAttribute";
-}
-
-function isEventHandler(attr: t.JSXAttribute): boolean {
-  return Boolean(getName(attr)?.match(/^on[A-Z]/));
-}
-
 function getName(attr: t.JSXAttribute): string {
   return attr.name.type === "Identifier" ? attr.name.value : attr.name.name.value;
 }
 
 function getIdentifiers(node: Node): t.Identifier[] {
   throw new Error("Not implemented: getIdentifiers");
-}
-
-function unique<T>(): (arr: T[]) => T[] {
-  return (arr: T[]) => Array.from(new Set(arr));
 }
