54ab6e3276519b312f70898131d315303fb951d9
Lukas Bombach <lukas.bombach@googlemail.com>
2024-12-25 21:19:52 +0100

    get functions

--- packages/pipe/getFunctions.ts
+++ packages/pipe/getFunctions.ts
@@ -1,7 +1,19 @@
+import { traverse } from "@juno/traverse";
 import type { Node } from "@juno/parse";
 
+type FunctionType = (typeof functionTypes)[number];
+
+const functionTypes = ["FunctionDeclaration", "FunctionExpression", "ArrowFunctionExpression"] as const;
+
 export function getFunctions() {
-  return (node: Node): Node<"FunctionDeclaration" | "FunctionExpression" | "ArrowFunctionExpression">[] => {
-    throw new Error("Not implemented: getFunctions");
+  return (node: Node): Node<FunctionType>[] => {
+    return traverse(node)
+      .map(([n]) => n)
+      .filter(isFunctionType)
+      .toArray();
   };
+}
+
+function isFunctionType(node: Node): node is Node<FunctionType> {
+  return functionTypes.includes(node.type as FunctionType);
 }
