5ea79f97a54d38c45009e0e53652c3ce278bc86f
Lukas Bombach <lukas.bombach@googlemail.com>
2024-12-27 15:46:17 +0100

    replaceWithHydrationJs wip

--- packages/pipe/replaceWithHydrationJs.ts
+++ packages/pipe/replaceWithHydrationJs.ts
@@ -1,8 +1,15 @@
 import { traverse } from "@juno/traverse";
+import { getJSXElements } from "./getJSXElements";
+import { filterInteractive } from "./filterInteractive";
+import { pipe } from "./pipe";
 import type { Node } from "@juno/parse";
 
 export function replaceWithHydrationJs() {
   return (nodes: Node<"JSXElement">[]): void => {
-    console.log(nodes.length);
+    nodes.forEach((node) => {
+      const x = pipe([node], getJSXElements(), filterInteractive());
+
+      console.log(x.map((p) => (p[0].opening.name as Node<"Identifier">).value));
+    });
   };
 }
