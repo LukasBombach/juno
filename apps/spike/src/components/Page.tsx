1483aa2790fe5c36f75e6116c11fdc65606e544d
Lukas Bombach <lukas.bombach@googlemail.com>
2025-01-03 13:25:39 +0100

    better css import

--- apps/spike/src/components/Page.tsx
+++ apps/spike/src/components/Page.tsx
@@ -9,11 +9,16 @@ export const Page: FC<{ children?: ReactNode; title?: string }> = (props) => {
         <meta charSet="UTF-8" />
         <link rel="icon" type="image/svg+xml" href="/vite.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/src/tailwind.css" />
         <title>{props.title} - juno</title>
       </head>
       <body>
-        <ListOfExamples />
-        <main>{props.children}</main>
+        <div className="container mx-auto px-4 grid grid-cols-[300px_1fr] gap-4">
+          <nav>
+            <ListOfExamples />
+          </nav>
+          <main>{props.children}</main>
+        </div>
       </body>
     </html>
   );
