a30d71d7f2b8ed3694add475090957bfa393e5d6
Lukas Bombach <lukas.bombach@googlemail.com>
2025-01-03 13:37:40 +0100

    readable menu

--- apps/spike/src/components/ListOfExamples.tsx
+++ apps/spike/src/components/ListOfExamples.tsx
@@ -2,19 +2,19 @@ import fs from "node:fs";
 
 import type { FC } from "react";
 
-function getPages(): string[] {
+function getPages(): [string, string][] {
   return fs
     .readdirSync(process.cwd() + "/src")
     .filter((file) => file.endsWith(".tsx"))
-    .map((file) => file.replace(/\.tsx$/, ""));
+    .map((file) => [file.replace(/\.tsx$/, ""), file.replace(/_/g, " ").replace(/\.tsx$/, "")]);
 }
 
 export const ListOfExamples: FC = () => {
   return (
     <ul>
-      {getPages().map((name) => (
-        <li key={name}>
-          <a href={`/${name}`}>{name}</a>
+      {getPages().map(([path, name]) => (
+        <li key={path}>
+          <a href={`/${path}`}>{name}</a>
         </li>
       ))}
     </ul>
