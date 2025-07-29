aff8a08a3036cdfd36d1b86491d7b44a2c41ef42
Lukas Bombach <lukas.bombach@googlemail.com>
2025-07-29 19:47:23 +0200

    function language worker

--- apps/homepage/src/components/Editor.tsx
+++ apps/homepage/src/components/Editor.tsx
@@ -1,12 +1,29 @@
 import React, { useRef, useEffect } from "react";
+import * as monaco from "monaco-editor";
 import { editor } from "monaco-editor/esm/vs/editor/editor.api";
+import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
 export interface EditorProps {
   value?: string;
   className?: string;
 }
 
-export const Editor: React.FC<EditorProps> = ({ value = "", className }) => {
+self.MonacoEnvironment = {
+  getWorker(_: any, label: string) {
+    if (label === "typescript" || label === "javascript") {
+      return new tsWorker();
+    }
+    return new editorWorker();
+  },
+};
+
+monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
+
+export const Editor: React.FC<EditorProps> = ({
+  value = ["function x() {", '\tconsole.log("Hello world!");', "}"].join("\n"),
+  className,
+}) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
 
