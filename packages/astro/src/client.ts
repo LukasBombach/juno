8f6d7365986f8a9835455c8d1832244874435d72
Lukas Bombach <lukas.bombach@googlemail.com>
2025-07-31 22:58:15 +0200

    wip

--- packages/astro/src/client.ts
+++ packages/astro/src/client.ts
@@ -1,4 +1,4 @@
 export default (element: HTMLElement) =>
   async (...args: unknown[]) => {
-    console.log("juno hydrate", element, ...args);
+    console.log("juno hydrate 111", element, ...args);
   };
