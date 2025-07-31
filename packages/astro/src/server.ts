96a9c5528ccceadd6ccb9fc0388997b0aca64b24
Lukas Bombach <lukas.bombach@googlemail.com>
2025-07-31 22:26:43 +0200

    server spike

--- packages/astro/src/server.ts
+++ packages/astro/src/server.ts
@@ -4,6 +4,18 @@ const check: AsyncRendererComponentFn<boolean> = async () => {
   return true;
 };
 
+const renderToStaticMarkup: AsyncRendererComponentFn<{
+  html: string;
+  attrs?: Record<string, string>;
+}> = async () => {
+  return {
+    html: "<div>Hello, Juno!</div>",
+    attrs: {
+      "data-juno": "true",
+    },
+  };
+};
+
 const renderer: NamedSSRLoadedRendererValue = {
   name: "juno",
   check,
