873c86d4b94d3d5cc276b8ea23ed06e451117c32
Lukas Bombach <lukas.bombach@googlemail.com>
2025-08-04 21:20:54 +0200

    jsx-runtime is good

--- packages/juno-astro/src/jsxRuntime.ts
+++ packages/juno-astro/src/jsxRuntime.ts
@@ -1,37 +1,71 @@
-import type { ReactNode, ReactElement } from "react";
-
-/*
- * Aliases to make how react / jsx provides / needs as inputs
- * and outputs more explicit
+/**
+ * @fileoverview
+ * This file exports various methods that implement Babel's "automatic" JSX runtime API:
+ * - jsx(type, props, key)
+ * - jsxs(type, props, key)
+ * - jsxDEV(type, props, key, __source, __self)
+ *
+ * Types are gratefully copied from preact/jsx-runtime
  */
-type TagOrComponent = string | Component;
-type VirtualDom = ReactElement;
 
-type Component = (props: Props) => VirtualDom;
-type Props = Record<string, any>;
+interface VNode<P = {}> {
+  type: FunctionComponent<P> | string;
+  props: P & { children: ComponentChildren };
+  key: Key;
+}
+
+interface FunctionComponent<P = {}> {
+  (props: RenderableProps<P>, context?: any): VNode<P>; // ComponentChildren; purposely ignoring the truth for easier development
+  displayName?: string;
+  defaultProps?: Partial<P> | undefined;
+}
+
+interface Attributes {
+  key?: Key | undefined;
+  jsx?: boolean | undefined;
+}
+
+interface Source {
+  fileName: string;
+  lineNumber: number;
+  columnNumber: number;
+}
+
+type Key = string | number | any;
+type RefObject<T> = { current: T | null };
+type RefCallback<T> = (instance: T | null) => void | (() => void);
+type Ref<T> = RefObject<T> | RefCallback<T> | null;
+type ComponentChildren = ComponentChild[] | ComponentChild;
+type ComponentChild = VNode<any> | object | string | number | bigint | boolean | null | undefined;
+type RenderableProps<P, RefType = any> = P &
+  Readonly<Attributes & { children?: ComponentChildren; ref?: Ref<RefType> }>;
 
 /**
- * Part of the JSX runtime paradigm.
- *
- * Factory function that will receive a transpiled JSX element
- * and returns virtual DOM.
+ * Creates a VNode for a component or HTML element.
+ * This function is used to implement the JSX runtime API.
+ * It takes a type (either a function component or a string for HTML elements),
+ * props (including children), a key, and a flag indicating if the children are static.
+ * It returns a VNode that represents the component or element.
  */
-function createElement(type: TagOrComponent, props: Props, ...children: ReactNode[]): VirtualDom {
-  if (typeof type === "function") {
-    return type({ ...props, children });
-  } else {
-    return {
-      type,
-      key: null,
-      props: {
-        ...props,
-        children: children.length === 1 ? children[0] : children,
-      },
-    };
-  }
+function createVNode<P = {}>(
+  type: FunctionComponent<P> | string,
+  props: P & { children: ComponentChildren },
+  key: Key,
+  _isStaticChildren: boolean,
+  _source: Source
+): VNode<P> {
+  return typeof type === "function" ? type(props) : { type, key, props };
 }
 
 /**
- * Expected export by vite/esbuild setting jsx: automatic
+ * Creates a fragment VNode for grouping children without adding extra nodes to the DOM.
+ * This is used to support JSX fragments in the automatic runtime.
  */
-export const jsxDEV = createElement;
+export function createFragmentVNode(props: VNode["props"]): VNode["props"]["children"] {
+  return props.children;
+}
+
+export const jsx = createVNode;
+export const jsxs = createVNode;
+export const jsxDEV = createVNode;
+export const Fragment = createFragmentVNode;
