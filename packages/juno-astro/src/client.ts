7e72fe5bd85e894bcbf90946b9ad2afa59fa5231
Lukas Bombach <lukas.bombach@googlemail.com>
2025-08-07 09:55:19 +0200

    editor juno wip

--- packages/juno-astro/src/client.ts
+++ packages/juno-astro/src/client.ts
@@ -1,6 +1,11 @@
 console.log("juno hydration script initialized");
 
 export default (element: HTMLElement) =>
-  async (...args: unknown[]) => {
-    console.log("hydrating", element, ...args);
+  async (
+    Component: any,
+    props: Record<string, any>,
+    { default: children, ...slotted }: Record<string, any>,
+    { client }: Record<string, string>
+  ) => {
+    console.log("hydrating", element, Component({ ...props, children }), Component, props, children, slotted, client);
   };
