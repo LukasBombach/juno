70acb49b699dff8f42084c8c41013ca5ecd5e56c
Lukas Bombach <lukas.bombach@googlemail.com>
2025-08-11 00:04:22 +0200

    pkajsdkljas

--- packages/juno-astro/src/clientTransform.ts
+++ packages/juno-astro/src/clientTransform.ts
@@ -1,9 +1,10 @@
 import { basename } from "node:path";
 import * as A from "fp-ts/Array";
+import * as O from "fp-ts/Option";
 import oxc from "oxc-parser";
-import { builders as b } from "estree-toolkit";
+import { builders as b, is } from "estree-toolkit";
 import { pipe, findAllByType, findAllByTypeShallow } from "juno-ast";
-import type { JSXElement } from "juno-ast";
+import type { JSXElement, JSXExpressionContainer, Expression } from "juno-ast";
 
 export function transformJsx(code: string, id: string) {
   const { program } = oxc.parseSync(basename(id), code, { sourceType: "module", lang: "tsx", astType: "js" });
@@ -22,6 +23,21 @@ export function transformJsx(code: string, id: string) {
   return code;
 }
 
-function createHydration(jsxRoot: JSXElement) {
-  return b.arrayExpression([]);
+function createHydration(jxElement: JSXElement) {
+  const path = [1];
+  const ref = pipe(
+    jxElement.openingElement,
+    findAllByType("JSXAttribute"),
+    A.findFirst(attr => is.jsxIdentifier(attr.name) && attr.name.name === "ref"),
+    O.map(attr => attr.value),
+    O.filter((v): v is JSXExpressionContainer => v?.type === "JSXExpressionContainer"),
+    O.map(v => v.expression),
+    O.filter(v => v.type !== "JSXEmptyExpression"),
+    O.map(v => b.property("init", b.identifier("ref"), v as any)),
+    O.toUndefined
+  );
+
+  return b.arrayExpression([
+    b.objectExpression([b.property("init", b.identifier("path"), b.arrayExpression(path.map(b.literal)))]),
+  ]);
 }
