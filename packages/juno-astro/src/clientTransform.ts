02c20a14c7ed8436832913da1bebe52e8a778541
Lukas Bombach <lukas.bombach@googlemail.com>
2025-08-10 23:32:47 +0200

    asd

--- packages/juno-astro/src/clientTransform.ts
+++ packages/juno-astro/src/clientTransform.ts
@@ -1,7 +1,9 @@
 import { basename } from "node:path";
 import * as A from "fp-ts/Array";
 import oxc from "oxc-parser";
+import { builders as b } from "estree-toolkit";
 import { pipe, findAllByType, findAllByTypeShallow } from "juno-ast";
+import type { JSXElement } from "juno-ast";
 
 export function transformJsx(code: string, id: string) {
   const { program } = oxc.parseSync(basename(id), code, { sourceType: "module", lang: "tsx", astType: "js" });
@@ -12,8 +14,14 @@ export function transformJsx(code: string, id: string) {
     program,
     findAllByType("FunctionDeclaration", "FunctionExpression", "ArrowFunctionExpression"),
     A.flatMap(findAllByTypeShallow("JSXElement")),
-    A.map(jsxReturnRoot => console.log(jsxReturnRoot))
+    A.map(jsxReturnRoot => {
+      console.log(jsxReturnRoot);
+    })
   );
 
   return code;
+}
+
+function createHydration(jsxRoot: JSXElement) {
+  return b.arrayExpression([]);
 }
