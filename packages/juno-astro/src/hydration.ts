d69ddadcab99d94a558d1334c5a273e061fceda5
Lukas Bombach <lukas.bombach@googlemail.com>
2025-08-23 10:12:31 +0200

    bad code but who cares

--- packages/juno-astro/src/hydration.ts
+++ packages/juno-astro/src/hydration.ts
@@ -1,18 +1,8 @@
-import { basename } from "node:path";
-import { createHash } from "node:crypto";
-import * as A from "fp-ts/Array";
-import * as O from "fp-ts/Option";
-import oxc from "oxc-parser";
-import { print } from "esrap";
-import tsx from "esrap/languages/tsx";
-// import { highlight } from "cli-highlight";
-// import c from "chalk";
-import { pipe, is, as, b, matches } from "juno-ast";
-import { findAllByType, findAllByTypeShallow, findFirstByType } from "juno-ast";
-import type { Node, NodeOfType, JSXElement } from "juno-ast";
+import { is, matches } from "juno-ast";
+import type { Node } from "juno-ast";
 
 export function windowAvailableCheck(node: Node): boolean {
-  const typeofWindow /*:  Partial<NodeOfType<"UnaryExpression">> */ = {
+  const typeofWindow = {
     type: "UnaryExpression",
     operator: "typeof",
     argument: {
@@ -21,5 +11,14 @@ export function windowAvailableCheck(node: Node): boolean {
     },
   };
 
-  return is.BinaryExpression(node) && [node.left, node.right].some(el => matches(typeofWindow)(el));
+  const undef = {
+    type: "Literal",
+    value: "undefined",
+  };
+
+  return (
+    is.BinaryExpression(node) &&
+    [node.left, node.right].some(el => matches(typeofWindow)(el)) &&
+    [node.left, node.right].some(el => matches(undef)(el))
+  );
 }
