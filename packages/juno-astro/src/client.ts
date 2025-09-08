ffda88b01c886542b98b31ce5b25fc081fe8afef
Lukas Bombach <lukas.bombach@googlemail.com>
2025-09-08 16:34:43 +0200

    wip

--- packages/juno-astro/src/client.ts
+++ packages/juno-astro/src/client.ts
@@ -24,16 +24,15 @@ export default (element: HTMLElement) =>
       const id = root.getAttribute("data-component-id");
       if (id && window.JUNO_COMPONENTS[id]) {
         const Comp = window.JUNO_COMPONENTS[id];
-        const hydrations: Hydration[] = await Comp({ ...props, children, ...slotted });
+        const hydrations: (Hydration | Hydration[])[] = await Comp({ ...props, children, ...slotted });
+
+        console.dir(hydrations);
 
         for (const h of hydrations) {
-          const el =
-            root.getAttribute("data-element-id") === h.id ? root : root.querySelector(`[data-element-id="${h.id}"]`);
-          if (el) {
-            for (const [name, value] of Object.entries(h)) {
-              if (name === "ref") value(el);
-              if (name.match(/^on[A-Z]/)) el.addEventListener(name.slice(2).toLowerCase(), value);
-            }
+          if (Array.isArray(h)) {
+            h.forEach((hh, i) => hydrateElement(root, hh, i));
+          } else {
+            hydrateElement(root, h, 0);
           }
         }
       } else {
@@ -42,3 +41,26 @@ export default (element: HTMLElement) =>
       }
     }
   };
+
+function hydrateElement(root: Element, h: Hydration, index: number) {
+  //const el = root.getAttribute("data-element-id") === h.id ? root : root.querySelector(`[data-element-id="${h.id}"]`);
+
+  const elements = [...root.querySelectorAll(`[data-element-id="${h.id}"]`)];
+  if (root.getAttribute("data-element-id") === h.id) {
+    elements.unshift(root);
+  }
+
+  const el = elements[index];
+
+  if (!el) {
+    console.warn("Cannot find element with id", h.id, "at index", index, "in", root);
+    return;
+  }
+
+  if (el) {
+    for (const [name, value] of Object.entries(h)) {
+      if (name === "ref") value(el);
+      if (name.match(/^on[A-Z]/)) el.addEventListener(name.slice(2).toLowerCase(), value);
+    }
+  }
+}
