9fc1c7d64de8d9150a9a8ba1bd47f579947300a0
Lukas Bombach <lukas.bombach@googlemail.com>
2025-09-18 22:05:38 +0200

    jetzt geht plötzlich alles

--- apps/homepage/src/examples/Counter.tsx
+++ apps/homepage/src/examples/Counter.tsx
@@ -1,11 +1,15 @@
 import { signal } from "@maverick-js/signals";
 
+function SomeTextInside() {
+  return <span>Julia ist lieP</span>;
+}
+
 export function Counter(props: { className?: string } = {}) {
   const count = signal(1);
 
   return (
     <button onClick={() => count.set(count() + 1)} className={props.className}>
-      Clicked {count()} {count() === 1 ? "time" : "times"}
+      Clicked {count()} <SomeTextInside /> {count() === 1 ? "time" : "times"}
     </button>
   );
 }
