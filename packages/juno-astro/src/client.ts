b4ec15b88f0dc37edc7490ea744fba7e3625a7af
Lukas Bombach <lukas.bombach@googlemail.com>
2025-09-28 19:22:06 +0200

    hydration wip

--- packages/juno-astro/src/client.ts
+++ packages/juno-astro/src/client.ts
@@ -28,6 +28,10 @@ type JsxExpressionFunction = () => string | number | unknown;
 
 type Hydration = ComponentHydration | ElementHydration;
 
+function isElementHydration(hydration: Hydration): hydration is ElementHydration {
+  return "elementId" in hydration;
+}
+
 function isComponentHydration(hydration: Hydration): hydration is ComponentHydration {
   return "component" in hydration;
 }
@@ -55,82 +59,98 @@ export default (element: HTMLElement) =>
     { default: children, ...slotted }: Record<string, any>,
     { client }: Record<string, string>
   ) => {
-    const elements = [...element.querySelectorAll("[data-element-id]")];
-    hydrate(component);
+    const hydrations = component({});
 
-    function hydrate(component: Component) {
-      const hydrations = component({});
-
-      for (const hydration of hydrations) {
-        if (isComponentHydration(hydration)) {
-          hydrate(hydration.component);
-        } else {
-          const el = elements.shift();
-
-          if (!el) {
-            console.warn("No more elements to hydrate for", hydration);
-            continue;
+    for (const hydration of hydrations) {
+      console.log(hydration);
+      if (isElementHydration(hydration)) {
+        for (const child of hydration.children || []) {
+          if (typeof child === "function") {
+            const result = child();
+            console.log(result);
           }
-
-          hydrateElement(hydration, el as HTMLElement);
         }
       }
     }
 
-    function hydrateElement(hydration: ElementHydration, el: HTMLElement) {
-      const { elementId, ref, children, ...eventHandlers } = hydration;
+    return;
 
-      /**
-       * Ensure that the element ids match
-       */
-      if (elementId !== el.getAttribute("data-element-id")) {
-        console.warn("Mismatched hydration id", elementId, el);
-        return;
-      }
-
-      /**
-       * Call ref with the element
-       */
-      if (ref) {
-        ref(el);
-      }
-
-      /**
-       * Add event listeners
-       */
-      for (const [name, value] of Object.entries(eventHandlers)) {
-        if (!name.match(/^on[A-Z]/)) {
-          console.warn("Unsupported hydration property", name, value);
-        } else {
-          el.addEventListener(name.slice(2).toLowerCase(), value as EventListener);
-        }
-      }
-
-      /**
-       * crazy algorithm to interleave static text nodes with dynamic ones
-       * based on the children array which contains numbers (static text length)
-       * and functions (dynamic text).
-       *
-       * We assume that the server rendered the exact same static text content
-       * as we have here, so we can split the text nodes at the correct indices.
-       */
-      let index = 0;
-      let text = el.firstChild;
-      asssertChildNode(text);
-      for (const child of children || []) {
-        if (typeof child === "number") {
-          index += child;
-        } else {
-          assertTextNode(text);
-          const currentText = text.splitText(index);
-          text = currentText.splitText(String(child()).length);
-          index = 0;
-          effect(() => {
-            currentText.textContent = String(child());
-          });
-        }
-      }
-    }
+    //    const elements = [...element.querySelectorAll("[data-element-id]")];
+    //    hydrate(component);
+    //
+    //    function hydrate(component: Component) {
+    //      const hydrations = component({});
+    //
+    //      for (const hydration of hydrations) {
+    //        if (isComponentHydration(hydration)) {
+    //          hydrate(hydration.component);
+    //        } else {
+    //          const el = elements.shift();
+    //
+    //          if (!el) {
+    //            console.warn("No more elements to hydrate for", hydration);
+    //            continue;
+    //          }
+    //
+    //          hydrateElement(hydration, el as HTMLElement);
+    //        }
+    //      }
+    //    }
+    //
+    //    function hydrateElement(hydration: ElementHydration, el: HTMLElement) {
+    //      const { elementId, ref, children, ...eventHandlers } = hydration;
+    //
+    //      /**
+    //       * Ensure that the element ids match
+    //       */
+    //      if (elementId !== el.getAttribute("data-element-id")) {
+    //        console.warn("Mismatched hydration id", elementId, el);
+    //        return;
+    //      }
+    //
+    //      /**
+    //       * Call ref with the element
+    //       */
+    //      if (ref) {
+    //        ref(el);
+    //      }
+    //
+    //      /**
+    //       * Add event listeners
+    //       */
+    //      for (const [name, value] of Object.entries(eventHandlers)) {
+    //        if (!name.match(/^on[A-Z]/)) {
+    //          console.warn("Unsupported hydration property", name, value);
+    //        } else {
+    //          el.addEventListener(name.slice(2).toLowerCase(), value as EventListener);
+    //        }
+    //      }
+    //
+    //      /**
+    //       * crazy algorithm to interleave static text nodes with dynamic ones
+    //       * based on the children array which contains numbers (static text length)
+    //       * and functions (dynamic text).
+    //       *
+    //       * We assume that the server rendered the exact same static text content
+    //       * as we have here, so we can split the text nodes at the correct indices.
+    //       */
+    //      let index = 0;
+    //      let text = el.firstChild;
+    //      asssertChildNode(text);
+    //      for (const child of children || []) {
+    //        if (typeof child === "number") {
+    //          index += child;
+    //        } else {
+    //          assertTextNode(text);
+    //          const currentText = text.splitText(index);
+    //          text = currentText.splitText(String(child()).length);
+    //          index = 0;
+    //          effect(() => {
+    //            currentText.textContent = String(child());
+    //          });
+    //        }
+    //      }
+    //    }
 
     // const hydrations = hydrate(component({}));
     //
