548fe0929abc2f9b203026c246d073a4f8733aa0
Lukas Bombach <lukas.bombach@googlemail.com>
2025-10-03 21:22:42 +0200

    dark mode

--- apps/homepage/src/components/Playground.tsx
+++ apps/homepage/src/components/Playground.tsx
@@ -23,16 +23,16 @@ export function Playground() {
 
   return (
     <div className="row-start-4 -row-end-2 col-start-2 -col-end-2 md:row-start-2 md:-row-end-2 md:col-start-4 md:-col-end-2 rounded-xl shadow-window grid grid-rows-1 grid-cols-[minmax(200px,14%)_1fr] overflow-hidden">
-      <nav className="p-7 flex flex-col gap-2 bg-neutral-200/80 border-r-1 border-r-neutral-950/10 ">
+      <nav className="p-7 flex flex-col gap-2 bg-neutral-200/80 border-r-1 border-r-neutral-950/10 dark:bg-neutral-900/70">
         {demos.map((demo, index) => (
           <button
             onClick={() => selectedDemo.set(index)}
-            className="text-sm cursor-pointer text-neutral-700 text-left hover:underline"
+            className="text-sm cursor-pointer text-neutral-700 text-left hover:underline dark:text-neutral-100"
           >
             {demo.title}
           </button>
         ))}
-        <Counter className="text-sm cursor-pointer text-neutral-700 text-left hover:underline" />
+        <Counter className="text-sm cursor-pointer text-neutral-700 text-left hover:underline dark:text-neutral-100" />
         {hydrationChallenge}
       </nav>
       <Editor
