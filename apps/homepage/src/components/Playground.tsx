b938bed3ac8291bb7ac7e95148c945e7fb7ac731
Lukas Bombach <lukas.bombach@googlemail.com>
2025-10-03 21:23:42 +0200

    cleanup

--- apps/homepage/src/components/Playground.tsx
+++ apps/homepage/src/components/Playground.tsx
@@ -1,4 +1,4 @@
-import { signal, effect } from "@maverick-js/signals";
+import { signal } from "@maverick-js/signals";
 import { Editor } from "./Editor";
 import { Counter } from "../examples/Counter";
 
@@ -17,10 +17,6 @@ const demos = [
 export function Playground() {
   const selectedDemo = signal(0);
 
-  effect(() => console.log("selected demo", selectedDemo()));
-
-  const hydrationChallenge = "do not put this static text in the client bundle";
-
   return (
     <div className="row-start-4 -row-end-2 col-start-2 -col-end-2 md:row-start-2 md:-row-end-2 md:col-start-4 md:-col-end-2 rounded-xl shadow-window grid grid-rows-1 grid-cols-[minmax(200px,14%)_1fr] overflow-hidden">
       <nav className="p-7 flex flex-col gap-2 bg-neutral-200/80 border-r-1 border-r-neutral-950/10 dark:bg-neutral-900/70">
@@ -33,7 +29,6 @@ export function Playground() {
           </button>
         ))}
         <Counter className="text-sm cursor-pointer text-neutral-700 text-left hover:underline dark:text-neutral-100" />
-        {hydrationChallenge}
       </nav>
       <Editor
         value={demos[selectedDemo()].code}
--- apps/homepage/src/examples/Counter.tsx
+++ apps/homepage/src/examples/Counter.tsx
@@ -1,4 +1,4 @@
-import { signal, effect } from "@maverick-js/signals";
+import { signal } from "@maverick-js/signals";
 
 function SomeTextInside() {
   return <span>Julia ist lieP</span>;
@@ -6,8 +6,6 @@ function SomeTextInside() {
 
 export function Counter(props: { className?: string } = {}) {
   const count = signal(1);
-
-  // effect(() => console.log("clicked", count(), count() === 1 ? "time" : "times"));
 
   return (
     <button onClick={() => count.set(count() + 1)} className={props.className}>
