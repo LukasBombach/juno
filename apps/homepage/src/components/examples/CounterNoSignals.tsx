f15bfd0008862d554e7d3be64b8e9bb51605f826
Lukas Bombach <lukas.bombach@googlemail.com>
2025-10-06 09:25:54 +0200

    wip

--- apps/homepage/src/components/examples/CounterNoSignals.tsx
+++ apps/homepage/src/components/examples/CounterNoSignals.tsx
@@ -1,8 +1,8 @@
-export function Counter(props: { className?: string } = {}) {
+export function Counter() {
   let count = 1;
 
   return (
-    <button onClick={() => count++} className={props.className}>
+    <button onClick={() => count++}>
       Clicked {count} {count === 1 ? "time" : "times"}
     </button>
   );
