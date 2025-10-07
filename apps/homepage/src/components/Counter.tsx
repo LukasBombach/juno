11fc0643c9b52cbfa5376dc52d72300263f4d8c0
Lukas Bombach <lukas.bombach@googlemail.com>
2025-10-07 21:51:12 +0200

    works

--- apps/homepage/src/components/Counter.tsx
+++ apps/homepage/src/components/Counter.tsx
@@ -1,4 +1,4 @@
-import { signal } from "@maverick-js/signals";
+import { signal } from "@preact/signals-core";
 
 function SomeTextInside() {
   return <span>Julia ist lieP</span>;
@@ -8,8 +8,8 @@ export function Counter(props: { className?: string } = {}) {
   const count = signal(1);
 
   return (
-    <button onClick={() => count.set(count() + 1)} className={props.className}>
-      Clicked {count()} {count() === 1 ? "time" : "times"}
+    <button onClick={() => count.value++} className={props.className}>
+      Clicked {count.value} {count.value === 1 ? "time" : "times"}
     </button>
   );
 }
