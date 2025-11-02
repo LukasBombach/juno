688a101a8671702ae733a364f84a0aeadd047412
Lukas Bombach <lukas.bombach@googlemail.com>
2025-11-02 13:03:31 +0100

    alt hydration structures

--- apps/homepage/src/components/Counter.expected.tsx
+++ apps/homepage/src/components/Counter.expected.tsx
@@ -11,6 +11,37 @@ export function Counter(props: { className?: string } = {}) {
     },
   ];
 }
+Counter.id = "a0ef";
+
+export function Counter2(props: { className?: string } = {}) {
+  const count = signal(1);
+
+  return [
+    [
+      ["click", () => count.value++],
+      ["className", props.className],
+      [() => count.value, () => (count.value === 1 ? "time" : "times")],
+    ],
+  ];
+}
+Counter2.id = "a0ef";
+
+export function Counter3(props: { className?: string } = {}) {
+  const count = signal(1);
+
+  return [
+    {
+      events: {
+        click: () => count.value++,
+      },
+      props: {
+        className: props.className,
+      },
+      children: [() => count.value, () => (count.value === 1 ? "time" : "times")],
+    },
+  ];
+}
+Counter3.id = "a0ef";
 
 export const CounterTemplate = `
   <button data-juno-element-id="a0ef-1">
