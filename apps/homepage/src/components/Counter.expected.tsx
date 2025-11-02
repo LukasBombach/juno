4104c789ce86e8069201554eeb63056bd0f51091
Lukas Bombach <lukas.bombach@googlemail.com>
2025-11-02 13:04:27 +0100

    wip

--- apps/homepage/src/components/Counter.expected.tsx
+++ apps/homepage/src/components/Counter.expected.tsx
@@ -13,36 +13,6 @@ export function Counter(props: { className?: string } = {}) {
 }
 Counter.id = "a0ef";
 
-export function Counter2(props: { className?: string } = {}) {
-  const count = signal(1);
-
-  return [
-    [
-      ["click", () => count.value++],
-      ["className", props.className],
-      [() => count.value, () => (count.value === 1 ? "time" : "times")],
-    ],
-  ];
-}
-Counter2.id = "a0ef";
-
-export function Counter3(props: { className?: string } = {}) {
-  const count = signal(1);
-
-  return [
-    {
-      events: {
-        click: () => count.value++,
-      },
-      props: {
-        className: props.className,
-      },
-      children: [() => count.value, () => (count.value === 1 ? "time" : "times")],
-    },
-  ];
-}
-Counter3.id = "a0ef";
-
 export const CounterTemplate = `
   <button data-juno-element-id="a0ef-1">
     Clicked <slot name="1"></slot> <slot name="2"></slot>
