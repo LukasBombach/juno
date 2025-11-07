420e9fa6c66afaa61558a176709636db9628cf31
Lukas Bombach <lukas.bombach@googlemail.com>
2025-11-07 16:58:54 +0100

    hydration format wip

--- apps/homepage/src/components/Counter.expected.tsx
+++ apps/homepage/src/components/Counter.expected.tsx
@@ -1,25 +1,30 @@
 import { signal } from "@preact/signals-core";
 
-export function Counter(props: { className?: string } = {}) {
+export function Counter() {
+  const count = signal(1);
+
+  return (
+    <button onClick={() => count.value++}>
+      Clicked {count.value} {count.value === 1 ? "time" : "times"}
+    </button>
+  );
+}
+
+export function CounterHydration() {
   const count = signal(1);
 
   return [
     {
       onClick: () => count.value++,
-      className: props.className,
       children: [() => count.value, () => (count.value === 1 ? "time" : "times")],
     },
   ];
 }
 
-export const CounterTemplate = `
-  <button>
-    Clicked <slot name="1"></slot> <slot name="2"></slot>
-  </button>
-`;
-
-export const CounterTemplate2 = (vars: any = {}) => `
-  <button class="${vars.className}">
-    Clicked ${vars.children[0]} ${vars.children[1]}
-  </button>
-`;
+export function CounterTemplate() {
+  return `
+    <button>
+      Clicked <slot name="1"></slot> <slot name="2"></slot>
+    </button>
+  `;
+}
