69485f1c4980a0f624ed993fcf3f89d3031d1ebf
Lukas Bombach <lukas.bombach@googlemail.com>
2025-11-09 21:49:36 +0100

    transformIndexHtml

--- packages/juno-vite/src/plugin.ts
+++ packages/juno-vite/src/plugin.ts
@@ -1,39 +1,27 @@
-import type { Plugin, IndexHtmlTransformResult, ResolvedConfig } from "vite";
+import type { Plugin, IndexHtmlTransformResult } from "vite";
 
-interface RouteDef {
-  path: string;
-  pattern: string; // regex source
-  loaderCode: string; // "() => import('...')"
-}
-
-export default function junoPlugin(
-  opts: {
-    pagesDir?: string;
-    extensions?: string[];
-  } = {}
-): Plugin {
-  const pagesDir = opts?.pagesDir ?? "src/pages";
-  const extensions = opts?.extensions ?? [".tsx", ".jsx", ".ts", ".js"];
-
-  let config: ResolvedConfig;
-  let routes: RouteDef[] = [];
-
-  const V_ROUTES = "virtual:file-router";
-  const V_CLIENT = "virtual:file-router/client";
-  const V_PREFIX = "\0"; // internal id prefix
-
+export default function junoPlugin(): Plugin {
   return {
     name: "vite-plugin-juno",
     enforce: "pre",
 
-    configResolved(c) {
-      config = c;
-      routes = buildRoutes({ root: c.root, pagesDir, extensions });
-    },
-
-    resolveId(source, importer, options) {
-      console.log("resolveId", { source });
-      return null;
+    transformIndexHtml(): IndexHtmlTransformResult {
+      return {
+        html: `
+          <!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>JUNRRRR</title>
+          </head>
+          <body>
+          junoooo  
+          </body>
+          </html>
+        `,
+        tags: [],
+      };
     },
   };
 }
