c3b174898c0130c800d7f379fada20aab122b9f2
Lukas Bombach <lukas.bombach@googlemail.com>
2025-11-09 22:44:02 +0100

    wip

---
+++ apps/vite-app/src/index.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <meta name="viewport" content="width=device-width" />
+        <title>juno</title>
+      </head>
+      <body>hello world</body>
+    </html>
+  );
+}
